dc9c95204f25ce8812c63dfd2ea0f552f6c6272c
shadowinhaze <shadowinhaze@gmail.com>
2021-08-06 20:05:13 +0300

    feat: add working function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checker = [];
+
+  checker = bracketsConfig.map((item) => {
+    return item.reduce((sum, e) => {
+      if (/\d/.test(e)) {
+        return sum + e;
+      } else {
+        return sum + '\\' + e;
+      }
+    }, '')
+  }).join('|');
+
+  let checkerApp = new RegExp(checker, 'g');
+  console.log(checkerApp)
+  while (checkerApp.test(str)) {
+    str = str.replace(checkerApp, '')
+  }
+
+  if (str.length === 0) {
+    return true
+  } else {
+    return false
+  };
 }
